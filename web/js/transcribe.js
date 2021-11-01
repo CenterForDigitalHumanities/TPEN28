960af6cf211cd52d8c4462fd5ae5b041841c9f12
Patrick Cuba <cubap@slu.edu>
2021-11-01 10:50:13 -0500

    IIIF shadow blasting
    It doubles the conditional, but would need to be rewritten a bit much to be cleaner.

--- web/js/transcribe.js
+++ web/js/transcribe.js
@@ -2205,6 +2205,12 @@ function drawLinesDesignateColumns(lines, tool, RTL, shift, preview, restore) {
         var lineHeight = theHeight * (height / 100) + "px";
         lineColumnIndicator.find('.lineColOnLine').attr("style", "line-height:" + lineHeight + ";");
         //Put to the DOM
+        if (hiResBackground.length>0){
+            lineColumnIndicator.css({
+                opacity:1,
+                boxShadow:"rgb(0 0 0 / 60%) 0px 0px 15px 0.5em"
+            })
+        }
         $(".lineColIndicatorArea").append(lineColumnIndicator);
         $("#fullpageSplitCanvas").append(fullPageLineColumnIndicator);
         colCounter++;

fb71751ddbabf6eb079c594897bb82663f92f28a
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2022-10-19 14:32:49 -0500

    Reject this

--- src/java/edu/slu/tpen/servlet/ProjectServlet.java
+++ src/java/edu/slu/tpen/servlet/ProjectServlet.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2014- Saint Louis University. Licensed under the
  *	Educational Community License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may
+ * not use this file except in compliances with the License. You may
  * obtain a copy of the License at
  *
  * http://www.osedu.org/licenses/ECL-2.0

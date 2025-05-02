9c9b3cebfeaaeb397749900a35c39b80be4e4582
Priyal Patel <mepripri0712@gmail.com>
2025-05-02 10:13:36 -0500

    Update GetProjectTPENServlet.java

--- src/java/edu/slu/tpen/servlet/GetProjectTPENServlet.java
+++ src/java/edu/slu/tpen/servlet/GetProjectTPENServlet.java
@@ -222,10 +222,11 @@ public class GetProjectTPENServlet extends HttpServlet {
                             jsonMap.put("xml", allProjectButtons);
                             //get special characters
                             jsonMap.put("projectButtons", hk.javascriptToAddProjectButtonsRawData(projectID));
-                            response.setHeader("Access-Control-Allow-Headers", "*");
-                            response.setHeader("Access-Control-Expose-Headers", "*"); //Headers are restricted, unless you explicitly expose them.  Darn Browsers.
+                            response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+                            response.setHeader("Access-Control-Expose-Headers", "Content-Type, Authorization"); //Headers are restricted, unless you explicitly expose them.  Darn Browsers.
                             //response.setHeader("Etag", request.getContextPath() + "/getProjectTPENServlet/project/"+proj.getProjectID());
                             response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+                            response.setHeader("Access-Control-Allow-Credentials", "true");
                             response.setHeader("Cache-Control", "max-age=15, must-revalidate");
                             try{
                                 String lastModifiedDateProj = proj.getModification().toString().replace(" ", "T");

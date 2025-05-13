ca808484b9ec0db91f14146d3b9afc1be12bdf8c
Bryan Haberberger <bryan.j.haberberger@slu.edu>
2025-05-13 10:06:25 -0500

    Adjustment to headers

--- src/java/edu/slu/tpen/servlet/GetProjectTPENServlet.java
+++ src/java/edu/slu/tpen/servlet/GetProjectTPENServlet.java
@@ -222,8 +222,9 @@ public class GetProjectTPENServlet extends HttpServlet {
                             jsonMap.put("xml", allProjectButtons);
                             //get special characters
                             jsonMap.put("projectButtons", hk.javascriptToAddProjectButtonsRawData(projectID));
-                            response.setHeader("Access-Control-Allow-Headers", "*");
-                            response.setHeader("Access-Control-Expose-Headers", "*"); //Headers are restricted, unless you explicitly expose them.  Darn Browsers.
+                            response.setHeader("Access-Control-Allow-Headers", "Etag,Location,Content-Type,Content-Length,Date,Authorization");
+                            response.setHeader("Access-Control-Expose-Headers", "Etag,Location,Content-Type,Content-Length,Date,Authorization"); //Headers are restricted, unless you explicitly expose them.  Darn Browsers.
+                            response.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
                             //response.setHeader("Etag", request.getContextPath() + "/getProjectTPENServlet/project/"+proj.getProjectID());
                             response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
                             response.setHeader("Access-Control-Allow-Credentials", "true");
